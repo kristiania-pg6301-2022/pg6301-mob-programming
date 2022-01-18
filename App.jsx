d0a34551935784c2cebdf82d4ca86e2ae986ce98
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2022-01-18 17:22:00 +0100

    list movies

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import {useState} from "react";
 
 function App() {
-    const [question, setQuestion] = useState('')
-    const [answer, setAnswer] = useState('')
+const [question, setQuestion] = useState("")
+const [answer, setAnswer] = useState("")
 
-    return <div>
 
-    </div>
+    return <div><h1>hey</h1></div>
 
 }
+
 export default App;
--- index.jsx
+++ index.jsx
@@ -1,6 +1,57 @@
-import ReactDOM from "react-dom"
 import React from "react"
-import App from "./App";
+import ReactDOM from "react-dom"
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<App/>, document.getElementById("app"))
 
+const movies = [
+    {
+        title: "The Matrix",
+        plot: "Flying monkeys 90s SiFi shit",
+        year: 1999
+    },
+    {
+        title: "Harry Potter",
+        plot: "Widard",
+        year: 2001
+    }
+
+]
+
+
+function FrontPage() {
+    return <div>
+        <h1>Kristiana movie database</h1>
+        <ul>
+            <li><Link to={"/movies"}>List movies</Link></li>
+            <li><Link to={"/movies/new"}>New movie</Link></li>
+        </ul>
+    </div>
+}
+
+function ListMovies() {
+    return <div><h1>List movies</h1>
+
+            {movies.map(m =>
+                <>
+                <h2>{m.title} ({m.year})</h2>
+                <div>{m.plot}</div>
+                </>
+            )}
+
+
+
+    </div>;
+
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage/>}/>
+            <Route path={"/movies/new"} element={<h1>New movie</h1>}/>
+            <Route path={"/movies"} element={<ListMovies/>}/>
+        </Routes>
+    </BrowserRouter>
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"))
