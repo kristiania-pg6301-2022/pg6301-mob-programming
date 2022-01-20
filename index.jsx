bc9873d19bc6711b30b70c2da10ec8a13423d300
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-01-20 18:16:54 +0100

    set up components for questions and answers

--- index.jsx
+++ index.jsx
@@ -1,12 +1,37 @@
 import ReactDOM from "react-dom"
 import React, {useState} from "react";
 import { BrowserRouter, Link, Routes, Route} from "react-router-dom";
+import{randomQuestion} from "./questions-1";
+
+function FrontPage() {
+    return <div>
+        <h1>Front-Page</h1>
+        <Link to={"/Question"}>New Quiz</Link>
+    </div>
+
+}
+
+function Quiz() {
+
+
+    const q  = randomQuestion();
+    const answers = Object.keys(q.answers).filter( a => q.answers[a] );
+    return <div>
+
+        <h1>Questions</h1>
+        <p>{q.question}</p>
+
+        {answers.map( (propKey)  =>{
+            return <button key={propKey}>{q.answers[propKey]}</button>
+        })}
+    </div>
+}
 
 function App() {
     return <BrowserRouter>
         <Routes>
-            <Route path="/" element={<h1>Front-Page</h1>}></Route>
-            <Route path={"/Question"} element={<h1>Question</h1>}> </Route>
+            <Route path="/" element={<FrontPage/>}></Route>
+            <Route path={"/Question"} element={<Quiz/>}> </Route>
             <Route path={"/answer/*"} element={<h1>answer</h1>}></Route>
         </Routes>
     </BrowserRouter>
