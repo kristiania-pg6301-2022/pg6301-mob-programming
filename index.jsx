aa56741e0acf4a35dd79be431991e75d7cf730c6
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-01-20 18:02:06 +0100

    Router set up with dummypages

--- index.jsx
+++ index.jsx
@@ -1,4 +1,16 @@
 import ReactDOM from "react-dom"
 import React, {useState} from "react";
-ReactDOM.render(<h1>hello world</h1>, document.getElementById("App"))
+import { BrowserRouter, Link, Routes, Route} from "react-router-dom";
+
+function App() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<h1>Front-Page</h1>}></Route>
+            <Route path={"/Question"} element={<h1>Question</h1>}> </Route>
+            <Route path={"/answer/*"} element={<h1>answer</h1>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
+
+ReactDOM.render(<App/>, document.getElementById("App"))
 
