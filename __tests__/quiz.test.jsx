41fc43ea0d75cd2c9399496b0e1b7353aa9f5c15
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2022-01-27 18:47:14 +0100

    added prettier

--- __tests__/quiz.test.jsx
+++ __tests__/quiz.test.jsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import * as ReactDOM from 'react-dom';
-import {Question} from "../question";
+import * as ReactDOM from "react-dom";
+import { Question } from "../question";
 
 describe("Quiz Application", () => {
-    it("shows quiz", () => {
-        const element = document.createElement("div")
-        ReactDOM.render(<Question />, element)
-        expect(element.innerHTML).toMatchSnapshot()
-    })
-})
+  it("shows quiz", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(<Question />, element);
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+});
--- index.jsx
+++ index.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from 'react-dom';
-import {Question} from "./question";
+import * as ReactDOM from "react-dom";
+import { Question } from "./question";
 
-
-ReactDOM.render(<Question/>, document.getElementById("app"));
+ReactDOM.render(<Question />, document.getElementById("app"));
