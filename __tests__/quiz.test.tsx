dc4d9295dc9de77bac03ef000404bf96c577f0f9
Ellaoui94 <abab010@student.kristiania.no>
2022-02-07 15:34:39 +0100

    run format test

--- __tests__/quiz.test.tsx
+++ __tests__/quiz.test.tsx
@@ -3,9 +3,8 @@ import * as ReactDOM from "react-dom";
 import { Question } from "../question";
 
 describe("Quiz Application", () => {
-  it("shows quiz", () => {
-    const element = document.createElement("div");
-    ReactDOM.render(<Question />, element);
-    expect(element.innerHTML).toMatchSnapshot();
+  it("shows quiz", () => { const element = document.createElement("div")
+    ReactDOM.render(<Question />, element)
+    expect(element.innerHTML).toMatchSnapshot()
   });
 });
