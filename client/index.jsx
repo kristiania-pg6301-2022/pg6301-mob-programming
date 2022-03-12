4f49f095681a049f4d5e8276263e3f4dfa7dc95e
Ellaoui94 <abab010@student.kristiania.no>
2022-03-12 16:27:18 +0100

    added components

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function FrontPage() {
+    return (
+        <div>
+
+            <h1>FrontPage</h1>
+            <div>
+                <Link to={"/login"}>Log in</Link>
+            </div>
+
+            <div>
+                <Link to={"/profile"}>Profile</Link>
+            </div>
+
+        </div>);
+}
+
+function Login() {
+    return null;
+}
+
+function Profile() {
+    return null;
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/profile"} element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>);
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
