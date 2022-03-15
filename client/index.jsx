b62cfebb5f67139d226ee35d488f210dd0a9d59d
Ellaoui94 <abab010@student.kristiania.no>
2022-03-15 19:59:25 +0100

    Added prettier, and login now can send you to Callback

--- client/index.jsx
+++ client/index.jsx
@@ -1,53 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function FrontPage() {
-    return (
+  return (
+    <div>
+      <h1>FrontPage</h1>
+      <div>
+        <Link to={"/login"}>Log in</Link>
+      </div>
+
+      <div>
+        <Link to={"/profile"}>Profile</Link>
         <div>
-
-            <h1>FrontPage</h1>
-            <div>
-                <Link to={"/login"}>Log in</Link>
-            </div>
-
-            <div>
-                <Link to={"/profile"}>Profile</Link>
-            </div>
-
-        </div>);
+          <a href={"/login/callback"}>sdsd</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-async function fetchJSON(url, options){
-    const res = await fetch(url, options)
-    if (!res.ok){
-        throw new Error(`Failed ${res.status}`)
-    }
-    return await res.json()
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+  return await res.json();
 }
 
 function Login() {
-    return null;
+  const [redirectURL, setRedirectURL] = useState();
+  useEffect(async () => {
+    const { authorization_endpoint } = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+
+    const parameters = {
+      response_type: "token",
+      client_id:
+        "134548719651-0n3bfj6iktgap06pmnfqj7gmvm849pm5.apps.googleusercontent.com",
+      scope: "email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
+
+    setRedirectURL(
+      authorization_endpoint + "?" + new URLSearchParams(parameters)
+    );
+  }, []);
+  return (
+    <div>
+      <a href={redirectURL}>Login</a>
+    </div>
+  );
 }
 
 function Profile() {
-    return null;
+  return null;
 }
 
 function Callback() {
-    return null;
+  return (
+    <div>
+      <h1>Callback</h1>
+    </div>
+  );
 }
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<FrontPage/>}/>
-                <Route path={"/login"} element={<Login/>}/>
-                <Route path={"/profile"} element={<Profile/>}/>
-                <Route path={"/login/callback"} element={<Callback/>}/>
-            </Routes>
-        </BrowserRouter>);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/login/callback"} element={<Callback />} />
+        <Route path={"/profile"} element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -9,27 +9,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-async function fetchJSON(url, options){
-    const res = await fetch(url, options)
-    if (!res.ok){
-        throw new Error(`Failed ${res.status}`)
-    }
-    return await res.json()
+async function fetchJSON(url, options) {
+  const res = await fetch(url, options);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+  return await res.json();
 }
 
 //
 
-
-
 app.use(express.static("../client/dist"));
 app.use((req, res, next) => {
-    if (req.method === "GET" && !req.path.startsWith("/api")) {
-        res.sendFile(path.resolve("..client/dist/index.html"))
-    }else {
-        next();
-    }
-})
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Started on http://localhost:${server.address().port}`);
+  console.log(`Started on http://localhost:${server.address().port}`);
 });
