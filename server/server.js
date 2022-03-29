3258fb0ede7951c874971310df26217991e9c7e9
Ellaoui94 <abab010@student.kristiania.no>
2022-03-30 00:28:37 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -1,33 +1,25 @@
 import express from "express";
 import * as path from "path";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import fetch from "node-fetch";
 import { WebSocketServer } from "ws";
-
-dotenv.config();
 
 const app = express();
 
-const wsServer = new WebSocketServer({ noServer: true });
+app.use(express.static("../client/dist"));
 
 const sockets = [];
+const wsServer = new WebSocketServer({ noServer: true });
+
 wsServer.on("connect", (socket) => {
   sockets.push(socket);
-  socket.send(JSON.stringify({ author: "j", message: "h" }));
   socket.on("message", (msg) => {
-    const { author, message } = JSON.parse(msg);
     for (const recipient of sockets) {
-      socket.send(JSON.stringify({ author, message }));
+      recipient.send(msg.toString());
     }
   });
 });
 
 app.use(bodyParser.json());
-app.use(cookieParser(process.env.COOKIE_SECRET));
-
-app.use(express.static("../client/dist"));
 app.use((req, res, next) => {
   if (req.method === "GET" && !req.path.startsWith("/api")) {
     res.sendFile(path.resolve("../client/dist/index.html"));
